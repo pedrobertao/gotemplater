359403e6527ab94d9485186d2633160616405d05
Pedro Bertao <pedrobertao11@hotmail.com>
2025-05-16 21:10:24 -0300

    :art: Pix pkg

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 	"log"
 	"path/filepath"
 
-	"github.com/pedrobertao/gotemplater/core/generator"
+	"github.com/pedrobertao/gotemplater/internal/generator"
 )
 
 func main() {
 	project := flag.String("name", "myapp", "Name of the project to generate")
-	template := flag.String("template", "api-simple", "Template to use (must exist in /templates)")
+	template := flag.String("template", "basic_web", "Template name (e.g., basic_web, web_with_sqlite)")
 	flag.Parse()
 
 	templatePath := filepath.Join("templates", *template, "structure.yaml")
 
 	if err := generator.Generate(*project, templatePath); err != nil {
-		log.Fatalf("Error generating project: %v", err)
+		log.Fatalf("❌ Error generating project: %v", err)
 	}
 
 	fmt.Printf("✅ Project '%s' generated using '%s' template!\n", *project, *template)
